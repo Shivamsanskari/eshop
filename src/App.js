babb171b616ea3171c23d1d8f9578b7659b46abb
shivamsanskari <shivamm733@gmail.com>
2023-01-18 20:16:23 +0530

    test change

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
       <BrowserRouter>
           <Header />
             <Routes>
-              
               <Route path="/" element={<Home />}></Route>
               <Route path="/contact" element={ <Contact /> }></Route>
             </Routes>
